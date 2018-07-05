07d5b8c71d181a790a9bd753b3196612e241a33f
Abiral Sthapit <abi.sthapit@gmail.com>
2018-07-05 12:19:46 +0545

    added the base_url

--- src/Config/enviroment.js
+++ src/Config/enviroment.js
@@ -1,2 +1,3 @@
-export const BASE_URL = 'http://localhost:4200/';
+// export const BASE_URL = 'http://localhost:4200/';
+export const BASE_URL = 'http://katiparxa.com:4300/';
 // export const BASE_URL = 'http://172.16.4.123:3000/';
