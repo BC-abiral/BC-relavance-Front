329bf87b64cbde9fd6fe8efed81d2fe2800de018
Abiral Sthapit <abi.sthapit@gmail.com>
2018-07-06 10:55:07 +0545

    removed ngrok

--- src/Config/enviroment.js
+++ src/Config/enviroment.js
@@ -1,8 +1,8 @@
 //My Local
 // export const BASE_URL = 'http://localhost:4200/';
-// export const BASE_URL = 'http://katiparxa.com:4300/';
+export const BASE_URL = 'http://katiparxa.com:4300/';
 //Rubin's port
 // export const BASE_URL = 'http://172.16.4.123:3000/';
 
 //ngrok access
-export const BASE_URL = 'http://0a63539b.ngrok.io'
+// export const BASE_URL = 'http://0a63539b.ngrok.io'
